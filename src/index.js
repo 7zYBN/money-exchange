f28659b1a36f239bf2b7f41b250cbee02aae91e6
7zYBN <frazo4ka@mail.ru>
2019-03-24 23:27:29 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	let object = {};
+	const halfDollarsWorth = 50,
+		quartersWorth = 25,
+		dimesWorth = 10,
+		nickelsWorth = 5;
+
+	if (currency > 0) {
+
+		if (currency > 10000) {
+			object.error = "You are rich, my friend! We don't have so much coins for exchange";
+		} else {
+			object.H = Math.floor(currency / halfDollarsWorth);
+			object.Q = Math.floor((currency % halfDollarsWorth) / quartersWorth);
+			object.D = Math.floor(((currency % halfDollarsWorth) % quartersWorth) / dimesWorth);
+			object.N = Math.floor((((currency % halfDollarsWorth) % quartersWorth) % dimesWorth) / nickelsWorth);
+			object.P = (((currency % halfDollarsWorth) % quartersWorth) % dimesWorth) % nickelsWorth;
+
+			for (key in object) {
+				if (object[key] == 0) {
+					delete object[key];
+				}
+			}
+
+		}
+	}
+
+	return object;
 }
